8c44da6da888b0e26de1829942f15c4829e231be
maiaheffernan <maia.bond.heffernan@gmail.com>
2026-07-24 16:34:31 -0700

    added more plotting and some cleaning to the soloT readin data

--- Sensors/RBR_soloT/SoloT_ReadinAndQuicklook.m
+++ Sensors/RBR_soloT/SoloT_ReadinAndQuicklook.m
@@ -19,9 +19,9 @@ solo_SNs = { ...
     {'212861', '212864', '212865', '212866', '212863', '212867', '212862'}, ...  % LoveJoyNorth
     {'212833', '212834', '212857', '212858', '212859', '212860', '212832'}, ...  % LoveJoySouth
     {'212868', '212869', '212870', '212871', '212872', '212873', '212874'}, ...  % InnerNorth
-    {'212875', '212876', '212877', '212878', '240151', '212879', '212880', '212881'} ...   % InnerSouth
+    {'212875', '212876', '212877', '212878', '212879', '212880', '212881'} ...   % InnerSouth
     };
-months_dir  = 'MaytoJun2026';  % used for the DIRECTORY name (includes year)
+months_dir  = 'JunJul2026';  % used for the DIRECTORY name (includes year)
 
 soloT_data = struct();  % top-level struct; one field per mooring
 
@@ -60,9 +60,11 @@ end
 
 % ===== put in the start and end times =====
 
-startTime = datetime(2026, 5, 27, 0, 0, 0);
-endTime = datetime(2026, 6, 23, 20, 40, 0);
+startTime = datetime(2026, 6, 25, 19, 0, 0);
+endTime = datetime(2026, 7, 21, 16, 30, 0);
 
+startTime_dn =datenum(startTime);
+endTime_dn =datenum(endTime);
 
 moorings = {'LoveJoyNorth', 'LoveJoySouth', 'InnerNorth', 'InnerSouth'};
 
@@ -92,4 +94,74 @@ end
 
 xlabel('Time');
 linkaxes(ax, 'xy');
-xlim(ax(1), [startTime, endTime]);
+
+xlim(ax(1), [startTime endTime]);
+
+%% plot histograms of the data to see the distribution
+
+figure;
+ax = gobjects(length(moorings), 1);
+
+for i = 1:length(moorings)
+    moorName = moorings{i};
+    ax(i) = subplot(length(moorings), 1, i);
+    hold on
+
+    snFields = fieldnames(soloT_data.(moorName));
+
+    for j = 1:length(snFields)
+        sn = snFields{j};
+        thisSensor = soloT_data.(moorName).(sn);
+
+        
+        histogram(thisSensor.data.values, 'DisplayName', sn);
+    end
+
+    title(moorName, 'Interpreter', 'none');
+    xlabel('Temp (\circC)');
+    legend('show', 'Location', 'eastoutside');
+    grid on
+end
+
+linkaxes(ax, 'xy');
+
+%% save the raw data
+
+save InnerSouth_JunJul2026_soloT_raw.mat soloT_data
+
+%% remove the data points before startTime and after endTime
+
+for i = 1:length(moorings)
+    moorName = moorings{i};
+    snFields = fieldnames(soloT_data.(moorName));
+    
+    for j = 1:length(snFields)
+        sn = snFields{j};
+        thisSensor = soloT_data.(moorName).(sn);
+        
+        % Remove data points outside the specified time range
+        validIndices = thisSensor.data.tstamp >= startTime_dn & thisSensor.data.tstamp <= endTime_dn;
+        thisSensor.data.values = thisSensor.data.values(validIndices);
+        thisSensor.data.tstamp = thisSensor.data.tstamp(validIndices);
+        
+        soloT_data.(moorName).(sn) = thisSensor;  % Update the struct with filtered data
+    end
+end
+
+
+%% clean the temperature data by de-spiking with 
+
+% Apply a de-spiking algorithm to the temperature data
+for i = 1:length(moorings)
+    moorName = moorings{i};
+    snFields = fieldnames(soloT_data.(moorName));
+    
+    for j = 1:length(snFields)
+        sn = snFields{j};
+        thisSensor = soloT_data.(moorName).(sn);
+        
+        % De-spike the temperature values
+        thisSensor.data.values = despike(thisSensor.data.values);
+        soloT_data.(moorName).(sn) = thisSensor;  % Update the struct with cleaned data
+    end
+end
