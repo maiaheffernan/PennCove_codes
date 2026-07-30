449c488173a4c613830a7ac9850b7262f1f0de24
maiaheffernan <maia.bond.heffernan@gmail.com>
2026-07-30 02:55:29 -0700

    edited plotting scripts for the velocity quiver plots and the timeseries plots.

--- Analysis/Timeseries_TideStratDOVel.m
+++ Analysis/Timeseries_TideStratDOVel.m
@@ -34,6 +34,11 @@ MayJun_LJNsig = load("SWIFT28_SDcard_MayJun2026_SIG.mat");
 JunJul_LJNsig = load("SWIFT28_SDcard_JunJul2026_SIG.mat");
 
 
+% velocity east/west from LJS, SWIFT 27
+
+MayJun_LJSsig = load("SWIFT27_SDcard_MayJun2026_SIG.mat");
+JunJul_LJSsig = load("SWIFT27_SDcard_JunJul2026_SIG.mat");
+
 %% concatenate variables
 
 months = {'MayJun', 'JunJul'};
@@ -153,6 +158,8 @@ end
 
 % === Signature ===
 
+% SWIFT 28
+
 allMonths4.MayJun = MayJun_LJNsig;
 allMonths4.JunJul = JunJul_LJNsig;
 
@@ -187,13 +194,47 @@ for i = 1:length(months)
 end
 
 
+% SWIFT 27
+
+allMonths5.MayJun = MayJun_LJSsig;
+allMonths5.JunJul = JunJul_LJSsig;
+
+% Initialize output arrays
+LJSsig.time = [];
+LJSsig.u    = [];
+LJSsig.v    = [];
+LJSsig.z    = [];   % filled once, since z is constant across time
+
+for i = 1:length(months)
+    monthName   = months{i};
+    SIG = allMonths5.(monthName).SIG;   % 1x1022 struct array
+
+    % --- time: one scalar per struct element -> 1022x1 vector ---
+    t = [SIG.time]';   % [SIG.time] gives 1x1022 row, transpose to column
+
+    % --- u and v: one 40x1 profile per element -> build 1022x40 matrix ---
+    u_cells = arrayfun(@(s) s.profile.u(:)', SIG, 'UniformOutput', false); % each 1x40
+    v_cells = arrayfun(@(s) s.profile.v(:)', SIG, 'UniformOutput', false);
+    u_mat = vertcat(u_cells{:});  % 1022 x 40
+    v_mat = vertcat(v_cells{:});  % 1022 x 40
+
+    % Concatenate onto the running arrays (growing the time/row dimension)
+    LJSsig.time = [LJSsig.time; t];
+    LJSsig.u    = [LJSsig.u;    u_mat];
+    LJSsig.v    = [LJSsig.v;    v_mat];
+
+    % z is the same every time step and every month, so just grab it once
+    if isempty(LJSsig.z)
+        LJSsig.z = SIG(1).profile.z(:);   % 40x1
+    end
+end
 
 
 
 % === SWIFT data ===
 
-allMonths5.MayJun = MayJun_SWIFT28;
-allMonths5.JunJul = JunJul_SWIFT28;
+allMonths6.MayJun = MayJun_SWIFT28;
+allMonths6.JunJul = JunJul_SWIFT28;
 
 % Initialize output arrays
 LJNSWIFT.time = [];
@@ -203,7 +244,7 @@ LJNSWIFT.Temp    = [];
 
 for i = 1:length(months)
     monthName   = months{i};
-    SWIFT = allMonths5.(monthName).SWIFT;   % 1x262 struct array for MAyJun
+    SWIFT = allMonths6.(monthName).SWIFT;   % 1x262 struct array for MAyJun
 
     % --- one scalar per struct element -> 262x1 vector for MayJun ---
     t = [SWIFT.time]';   % transpose to column
@@ -233,6 +274,8 @@ bottomDO_MayJul.LoveJoySouth.Datetime = datetime(bottomDO_MayJul.LoveJoySouth.ti
 tide_MayJul.Datetime = datetime(tide_MayJul.time, 'ConvertFrom', 'datenum');
 
 LJNsig.Datetime = datetime(LJNsig.time, 'ConvertFrom', 'datenum');
+
+LJSsig.Datetime = datetime(LJSsig.time, 'ConvertFrom', 'datenum');
 %% get the data ready to plot
 
 
@@ -275,16 +318,17 @@ tide_MayJul.eta = [tide_MayJul.Pres] - mean([tide_MayJul.Pres], 'omitnan');
 
 
 figure(1); clf;
-t = tiledlayout(4,1, 'TileSpacing', 'compact', 'Padding', 'compact');
+t = tiledlayout(5,1, 'TileSpacing', 'compact', 'Padding', 'compact');
 
 % --- Tile 1: tidal timeseries ---
 s1 = nexttile;
-    p1 = plot(tide_MayJul.Datetime, tide_MayJul.eta, 'k-', 'LineWidth', 2);
+    p1 = plot(tide_MayJul.Datetime, tide_MayJul.eta, 'k.', 'MarkerSize', 5);
     ylabel('SSH (m)')
 
 % --- Tile 2: delta S timeseries at LJN ---
 s2 = nexttile;
-    p2 = plot(matchedTime, deltaSal, 'm*-', 'LineWidth', 2);
+    p2 = plot(matchedTime, deltaSal, 'm*', 'MarkerSize', 4);
+    ylim([0 15])
     ylabel('\delta S (PSU)')
 
 % --- Tile 3: bottom DO timeseries ---
@@ -298,19 +342,88 @@ s3 = nexttile;
     legend([p4 p5], 'LoveJoy north', 'LoveJoy south', 'Location', 'Southwest');
 
 % --- Tile 4: pcolor of velocity at LJN ---
-s4 = nexttile;
-    p6 = pcolor(LJNsig.Datetime, LJNsig.z, LJNsig.u');
-    shading flat
-    set(gca, 'YDir', 'reverse')
-    colormap(cmocean('balance'))
-    c = colorbar;
-    c.Label.String = 'East/west velocity (m/s)';
-    clim([-1 1])
-    ylabel('Depth (m)')
-    xlabel('Time')
 
-linkaxes([s1 s2 s3 s4], 'x')
+% getting rid of the smearing in time from previous plot
 
+                % dt = diff(LJNsig.Datetime);    % time should be a datetime or duration vector
+                % nominal_dt = median(dt);      % typical sampling interval
+                % gap_thresh = 10 * nominal_dt;   
+                % 
+                % gap_idx = find(dt > gap_thresh); % indices where a gap starts
+                % 
+                % % Build segment index ranges
+                % starts = [1; gap_idx + 1];
+                % ends   = [gap_idx; length(LJNsig.Datetime)];
+                % segments = arrayfun(@(s,e) s:e, starts, ends, 'UniformOutput', false);
+
+segments = {30:179, 193:1201};
+
+s4 = nexttile; % plotting the pcolor segments separately but on the same subplot to get rid of the smearing
+hold on
+clim_vals = [-0.5 0.5];  
+
+for s = 1:numel(segments)
+    idx = segments{s};
+    pcolor(LJNsig.Datetime(idx), LJNsig.z, LJNsig.u(idx, :)');
+end
+
+shading flat
+colormap(cmocean('balance'))  
+caxis(clim_vals)
+c = colorbar;
+c.Label.String = 'East/west velocity (m/s)';
+xlim([LJNsig.Datetime(1) LJNsig.Datetime(end)])   % lock x-axis to full record so gaps show as blank/grey
+ylim([0 25])
+set(gca,'YDir','reverse')   
+xlabel('Time')
+ylabel('Depth (m)')
+subtitle('LoveJoy north')
+set(gca, 'Color', [0.8 0.8 0.8]);
+
+% previous plotting code
+    % p6 = pcolor(LJNsig.Datetime, LJNsig.z, LJNsig.u');
+    % shading flat
+    % set(gca, 'YDir', 'reverse')
+    % colormap(cmocean('balance'))
+    % c = colorbar;
+    % c.Label.String = 'East/west velocity (m/s)';
+    % clim([-1 1])
+    % ylabel('Depth (m)')
+    % xlabel('Time')
+    % set(gca, 'Color', [0.7 0.7 0.7]);
+
+s4plus1 = nexttile;
+
+p6 = pcolor(LJSsig.Datetime, LJSsig.z, LJSsig.u');
+shading flat
+set(gca, 'YDir', 'reverse')
+colormap(cmocean('balance'))
+c = colorbar;
+c.Label.String = 'East/west velocity (m/s)';
+clim([-0.5 0.5])
+ylabel('Depth (m)')
+xlabel('Time')
+subtitle('LoveJoy south ')
+set(gca, 'Color', [0.7 0.7 0.7]);
+
+
+
+
+
+
+
+
+linkaxes([s1 s2 s3 s4 s4plus1], 'x')
+
+xticks_common = datetime(2026,5,27):days(7):datetime(2026,7,22);
+set([s1 s2 s3 s4 s4plus1], 'XTick', xticks_common)
+
+% fig = gca;
+% 
+% set(findall(fig, '-property', 'FontSize'), 'FontSize', 18)
+% set(findall(fig, '-property', 'LineWidth'), 'LineWidth', 1.5)
+% 
+% exportgraphics(fig, fullfile(outdir, 'poster_figure.png'), 'Resolution', 600)
 
 
 
@@ -339,11 +452,20 @@ JunJul_tide.d_clean.Datetime = datetime(JunJul_tide.d_clean.tstamp, 'ConvertFrom
 
 % sig
 
+    % swift 28
 t_all_sig = [JunJul_LJNsig.SIG.time]';
 dt_all_sig = datetime(t_all_sig, 'ConvertFrom', 'datenum');
 
 for j = 1:length(JunJul_LJNsig.SIG)
     JunJul_LJNsig.SIG(j).Datetime = dt_all_sig(j);
+end
+
+    % swift 27
+t_all_sigS = [JunJul_LJSsig.SIG.time]';
+dt_all_sigS = datetime(t_all_sigS, 'ConvertFrom', 'datenum');
+
+for j = 1:length(JunJul_LJSsig.SIG)
+    JunJul_LJSsig.SIG(j).Datetime = dt_all_sigS(j);
 end
 
 
@@ -395,16 +517,17 @@ JunJul_tide.d_clean.eta = [JunJul_tide.d_clean.values(:,2)] - mean([JunJul_tide.
 %% figure 2, just june to july
 
 figure(2); clf;
-t = tiledlayout(4,1, 'TileSpacing', 'compact', 'Padding', 'compact');
+t = tiledlayout(5,1, 'TileSpacing', 'compact', 'Padding', 'compact');
 
 % --- Tile 1: tidal timeseries ---
 s5 = nexttile;
-    p7 = plot(JunJul_tide.d_clean.Datetime, JunJul_tide.d_clean.eta, 'k-', 'LineWidth', 2);
+    p7 = plot(JunJul_tide.d_clean.Datetime, JunJul_tide.d_clean.eta, 'k.', 'MarkerSize', 5);
     ylabel('SSH (m)')
 
 % --- Tile 2: delta S timeseries at LJN ---
 s6 = nexttile;
-    p8 = plot(matchedTimejunjul, deltaSal_junjul, 'm*-', 'LineWidth', 2);
+    p8 = plot(matchedTimejunjul, deltaSal_junjul, 'm*', 'MarkerSize', 4);
+    ylim([0 15])
     ylabel('\delta S (PSU)')
 
 % --- Tile 3: bottom DO timeseries ---
@@ -428,6 +551,8 @@ s7 = nexttile;
         u_cells = arrayfun(@(s) s.profile.u(:)', SIG, 'UniformOutput', false);
         u_mat = vertcat(u_cells{:});
 
+
+
 % --- Tile 4: pcolor of velocity at LJN ---
 s8 = nexttile;
     p11 = pcolor(plotTime, plotZ, u_mat');
@@ -436,12 +561,41 @@ s8 = nexttile;
     colormap(cmocean('balance'))
     c = colorbar;
     c.Label.String = 'East/west velocity (m/s)';
-    clim([-1 1])
+    clim([-0.5 0.5])
     ylabel('Depth (m)')
     xlabel('Time')
+    subtitle('LoveJoy north')
+    set(gca, 'Color', [0.7 0.7 0.7]);
 
-linkaxes([s5 s6 s7 s8], 'x')
 
+% === organizing things for signature plotting ===
+        SIG2 = JunJul_LJSsig.SIG;
+        plotTime2 = [SIG2.Datetime]';
+        plotZ2 = SIG2(1).profile.z(:);
+        u_cells2 = arrayfun(@(s) s.profile.u(:)', SIG2, 'UniformOutput', false);
+        u_mat2 = vertcat(u_cells2{:});
+
+% --- Tile 5: pcolor of velocity at LJS ---
+s8plus1 = nexttile;
+    pcolor(plotTime2, plotZ2, u_mat2');
+    shading flat
+    set(gca, 'YDir', 'reverse')
+    colormap(cmocean('balance'))
+    c = colorbar;
+    c.Label.String = 'East/west velocity (m/s)';
+    clim([-0.5 0.5])
+    ylabel('Depth (m)')
+    xlabel('Time')
+    subtitle('LoveJoy south')
+    set(gca, 'Color', [0.7 0.7 0.7]);
+
+
+
+
+linkaxes([s5 s6 s7 s8 s8plus1], 'x')
+
+xticks_common = datetime(2026,6,2):days(7):datetime(2026,7,22);
+set([s5 s6 s7 s8 s8plus1], 'XTick', xticks_common)
 
 
 %% zooming in on choice times 
@@ -449,19 +603,19 @@ linkaxes([s5 s6 s7 s8], 'x')
 % Zoom in from June 27 16:51 to June 29 at 00:27
 
 figure(3); clf;
-t = tiledlayout(4,1, 'TileSpacing', 'compact', 'Padding', 'compact');
+t = tiledlayout(5,1, 'TileSpacing', 'compact', 'Padding', 'compact');
 
 % --- Tile 1: tidal timeseries ---
 s5 = nexttile;
-    p7 = plot(JunJul_tide.d_clean.Datetime, JunJul_tide.d_clean.eta, 'k-', 'LineWidth', 2);
+    p7 = plot(JunJul_tide.d_clean.Datetime, JunJul_tide.d_clean.eta, 'k.', 'MarkerSize', 5);
     % Zoom in on the specified time range
     xlim([datetime('2026-06-27 16:50'), datetime('2026-06-29 17:45')]); % 00:27
     ylabel('SSH (m)')
 
 % --- Tile 2: delta S timeseries at LJN ---
 s6 = nexttile;
-    p8 = plot(matchedTimejunjul, deltaSal_junjul, 'm*-', 'LineWidth', 2);
-    % Zoom in on the specified time range
+    p8 = plot(matchedTimejunjul, deltaSal_junjul, 'm*', 'MarkerSize', 4);
+    ylim([0 15])
     xlim([datetime('2026-06-27 16:50'), datetime('2026-06-29 17:45')]);
     ylabel('\delta S (PSU)')
 
@@ -479,7 +633,7 @@ s7 = nexttile;
     ylabel('Dissolved oxygen concentration (mg/L)');
     yline(2, 'k--', 'LineWidth', 1.5)
     hold off
-    legend([p9 p10], 'LoveJoy north', 'LoveJoy south', 'Location', 'Southwest')
+    legend([p9 p10], 'LoveJoy north', 'LoveJoy south', 'Location', 'Northwest')
 
 
 
@@ -498,13 +652,38 @@ s8 = nexttile;
     colormap(cmocean('balance'))
     c = colorbar;
     c.Label.String = 'East/west velocity (m/s)';
-    clim([-1 1])
+    clim([-0.5 0.5])
     % Zoom in on the specified time range
     xlim([datetime('2026-06-27 16:50'), datetime('2026-06-29 17:45')]);
     ylabel('Depth (m)')
     xlabel('Time')
+    subtitle('LoveJoy north')
+    set(gca, 'Color', [0.7 0.7 0.7]);
 
-linkaxes([s5 s6 s7 s8], 'x')
+
+
+
+% --- Tile 5: pcolor of velocity at LJN ---
+s8plus1 = nexttile;
+    p11 = pcolor(plotTime2, plotZ2, u_mat2');
+    shading flat
+    set(gca, 'YDir', 'reverse')
+    colormap(cmocean('balance'))
+    c = colorbar;
+    c.Label.String = 'East/west velocity (m/s)';
+    clim([-0.5 0.5])
+    % Zoom in on the specified time range
+    xlim([datetime('2026-06-27 16:50'), datetime('2026-06-29 17:45')]);
+    ylabel('Depth (m)')
+    xlabel('Time')
+    subtitle('LoveJoy south')
+    set(gca, 'Color', [0.7 0.7 0.7]);
+
+linkaxes([s5 s6 s7 s8 s8plus1], 'x')
+
+
+xticks_common = datetime(2026,6,27):hours(6):datetime(2026,6,29);
+set([s5 s6 s7 s8 s8plus1], 'XTick', xticks_common)
 
 
 
