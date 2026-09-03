1c6b462705ca061abda1b9d09e590109e3a7ba7e
maiaheffernan <maia.bond.heffernan@gmail.com>
2026-09-03 09:05:04 -0700

    Started a script for testing the hypothesis about the perigean pattern of DO concentration.

---
+++ Analysis/perigeanTrend_testing.m
@@ -0,0 +1,21 @@
+%%% This script tests the hypothesis that there is a perigean trend to DO
+%%% values that correspond to the perigean signal in the tidal amplitudes.
+
+% Maia Heffernan, September 2026
+
+clear all, close all
+
+%% Load in the bottom DO data from all the months of the deployment and 
+% the tide pressure gage data from our mooring
+
+
+% May/Jun 
+mayjun = load TODOdata_MayJun2026_raw.mat
+
+% Jun/Jul
+
+
+% Jul/Aug
+
+load TODOdata_MayJun2026_raw.mat
+
